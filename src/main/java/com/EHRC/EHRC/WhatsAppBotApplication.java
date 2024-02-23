790d29712088f12994476fa5c96047bea52d3168
10956155 <dhruv.sgit@gmail.com>
2024-02-23 12:55:49 +0530

    Added Project structure.

---
+++ src/main/java/com/EHRC/EHRC/WhatsAppBotApplication.java
@@ -0,0 +1,13 @@
+package com.EHRC.EHRC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsAppBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsAppBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/EHRC/EHRC/WhatsAppBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EHRC.EHRC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsAppBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
